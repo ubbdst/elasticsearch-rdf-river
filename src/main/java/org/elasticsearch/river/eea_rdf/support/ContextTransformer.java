ba597086ae0b8d3d2bb7a01668e84b40688ab15c
Hemed Ali <hru066@ubbhf0225453.klientdrift.uib.no>
2018-01-26 17:14:07 +0100

    Add abstact JSONLD ContextTransformer

--- src/main/java/org/elasticsearch/river/eea_rdf/support/ContextTransformer.java
+++ src/main/java/org/elasticsearch/river/eea_rdf/support/ContextTransformer.java
@@ -1,11 +1,31 @@
 package org.elasticsearch.river.eea_rdf.support;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import java.util.Map;
 
 
 /**
- * Transformeer for JSONLD context
+ * @author  Hemed Al Ruwehy
+ *
+ * Transformer for JSONLD context
  */
 abstract class ContextTransformer extends JsonFileLoader {
+
+    private static final String CONTEXT_KEY = "@context";
+    private static final String CONTEXT_ID = "@id";
+
+
+    public static String getContextId() {
+        return CONTEXT_ID;
+    }
+
+
+    public static String getContextKey() {
+        return CONTEXT_KEY;
+    }
+
 
     /**
      * Transforms JSONLD context to Map of key-value strings. This is easily portable
@@ -13,4 +33,18 @@ abstract class ContextTransformer extends JsonFileLoader {
      * @return a map of sting key-values
      */
     public abstract Map<String, String> transform();
+
+
+    /**
+     * Extract JSONLD context key as a JsonElement
+     */
+    public JsonElement extractContextElement(String context) {
+        JsonElement con = new JsonParser().parse(context);
+        if (con.isJsonObject() && con.getAsJsonObject().getAsJsonObject(getContextKey()) != null) {
+            return con.getAsJsonObject().getAsJsonObject(getContextKey());
+        }
+        throw new IllegalArgumentException("JSON context must have @context key");
+    }
+
+
 }
